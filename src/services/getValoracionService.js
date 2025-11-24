3a38a2af80bc3e28667c551691a14b883d97e439
Alejandro <alexchiko05@gmail.com>
2025-11-24 17:27:51 +0100

    getValoraciones

---
+++ src/services/getValoracionService.js
@@ -0,0 +1,16 @@
+import { valoraciones } from "../public/data/info.js";
+
+
+export function getValoracion(id) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const valoracion = valoraciones.find(v => v.id === id)?.puntuacion
+            if (valoracion) {
+                resolve(valoracion);
+            } else {
+                reject(`No hay Valoraciones con id ${id}`);
+            }
+        }, 900);
+    });
+}
+    
